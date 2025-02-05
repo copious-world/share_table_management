ec8e4668614d26452724d71522da0954930ab3ea
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2025-02-05 08:36:02 -0800

    prepare for use with copious-transition-apps

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,50 @@
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+const fs = require('fs')
 
+let test_path = `${__dirname}/proc_js_table_pipe`
 
-const ProcJSTableManager = require('../lib/node_types/table_sharing_node')
-
-
-let conf = {
+let default_conf = {
+    //
+    "table_sharing_class" : "../lib/node_types/table_sharing_node",
     "aging" : 1000,
     "age_window" : 1000000,
+    "uds_path" : test_path,  // socket path
+    "uds_path_count" : 4,
+    //
     "parent_command_conf" : {
-        "parent_com" : {}, // this is a OS SOCKET
-        "parent_response_handler" : {},
-        "accepted_topics" : []
+        "parent_com" : "message-relay-services", //  require("connect_module")  must have subscribe and unsubscrieb
+        "class_choice" : "message_relayer",
+        "parent_conf" : {
+            "test" : "test"
+        },
+        "parent_response_handler" : "parent_response_handler",  // require("parent_response_handler") must have get_supplier_action(topic,path,this)
+        "handler_conf" : {  // application specific
+            "logging" : "log_event",
+            "refresh" : "reload_data",
+            "shutdown" : "kill_procs"
+        },
+        "accepted_topics" : [
+            "logging",
+            "refresh",
+            "shutdown"
+        ]
+    }
+}
+
+let conf = default_conf
+if ( process.argv.length > 2 ) {
+    let conf_file = fs.readFileSync(process.argv[process.argv.length-1])
+    if ( conf_file ) {
+        conf = JSON.parse(conf_file.toString())
     }
 }
 
 
 
-let pjtm = new ProcJSTableManager(conf)
+const ManagerProc = (conf.table_sharing_class !== undefined) ? require(conf.table_sharing_class) : require('../lib/node_types/table_sharing_node')
+
+/*
+    let ClientOfParent = require(conf.parent_command_conf.parent_com)
+*/
+
+let pjtm = new ManagerProc(conf)
